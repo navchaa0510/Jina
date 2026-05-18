671aa029aa38c9aafa8fdc6d6215c1e631080a51
navchaa0510 <147077674+navchaa0510@users.noreply.github.com>
2026-05-18 12:25:09 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#pip install pyinstaller
+
+
+# In[2]:
+
+
+import tkinter as tk
+
+root=tk.Tk()
+canvas1=tk.Canvas(root, width=300, height=300)
+canvas1.pack()
+
+def hello():
+    label1=tk.Label(root, text="Hello World", fg="green", font=("helvetica", 12, "bold"))
+    canvas1.create_window(150, 200, window=label1)
+
+button1=tk.Button(text="Click Me!", command=hello, bg="red", fg="black")
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
